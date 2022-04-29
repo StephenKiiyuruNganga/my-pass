12c64d6a99ef5d07dcd030b550b2510edf055e23
StephenKiiyuruNganga <40911055+StephenKiiyuruNganga@users.noreply.github.com>
2022-04-29 10:26:37 +0300

    added generate password logic

--- main.py
+++ main.py
@@ -1,10 +1,41 @@
+from ntpath import join
 import tkinter
 from tkinter import messagebox
-
+import random
+import pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+
+def generate_password():
+    password_input.delete(0, tkinter.END)
+
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+               'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    no_of_letters = random.randint(8, 10)
+    no_of_symbols = random.randint(2, 4)
+    no_of_numbers = random.randint(2, 4)
+
+    random_letters = [random.choice(letters) for round in range(no_of_letters)]
+    random_symbols = [random.choice(symbols) for round in range(no_of_symbols)]
+    random_numbers = [random.choice(numbers) for round in range(no_of_numbers)]
+
+    password_list = random_letters + random_symbols + random_numbers
+
+    random.shuffle(password_list)
+
+    new_password = "".join(password_list)
+
+    password_input.insert(0, new_password)
+
+    pyperclip.copy(new_password)
+
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
 
 def save_data():
     website = website_input.get().strip(" ")
@@ -54,7 +85,8 @@ password_input = tkinter.Entry(width=21)
 password_input.grid(row=3, column=1)
 
 # buttons
-generate_btn = tkinter.Button(text="Generate Password", command=None)
+generate_btn = tkinter.Button(text="Generate Password",
+                              command=generate_password)
 generate_btn.grid(row=3, column=2)
 add_btn = tkinter.Button(text="Add", width=30, command=save_data)
 add_btn.grid(row=4, column=1, columnspan=2)
